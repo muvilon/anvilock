0ab851d6986156025b17dc9d85d78520832b814f
Ananta Krishnan B <anantukid@gmail.com>
2024-10-30 16:21:52 +0530

    [CHORE] CHANGELOG v0.0.5-alpha

--- config.h
+++ config.h
@@ -6,18 +6,32 @@
 #include <string.h>
 #include <toml.h>
 
-#define CONFIG_FILE_PATH "config.toml"
+#define CONFIG_FILE_PATH                                                                           \
+  ({                                                                                               \
+    const char* home = getenv("HOME");                                                             \
+    home ? (snprintf(_config_path, sizeof(_config_path), "%s/.config/anvilock/config.toml", home), \
+            _config_path)                                                                          \
+         : (fprintf(stderr, "HOME environment variable not set.\n"), NULL);                        \
+  })
 
+// Buffer to hold config file path
+static char  _config_path[256];
 static char* font_path = NULL;
-char         errbuf[200];
+static char  errbuf[200];
 
 // 1 - Success, 0 - Failure
 static int load_config()
 {
-  FILE* config_file = fopen(CONFIG_FILE_PATH, "r");
+  const char* config_path = CONFIG_FILE_PATH;
+  if (!config_path)
+  {
+    return 0;
+  }
+
+  FILE* config_file = fopen(config_path, "r");
   if (config_file == NULL)
   {
-    fprintf(stderr, "Failed to open config file: %s\n", CONFIG_FILE_PATH);
+    fprintf(stderr, "Failed to open config file: %s\n", config_path);
     return 0;
   }
 
@@ -33,23 +47,24 @@ static int load_config()
   toml_table_t* font_table = toml_table_in(config, "font");
   if (!font_table)
   {
-    fprintf(stderr, "Failed to find font table in config file: %s\n", CONFIG_FILE_PATH);
+    fprintf(stderr, "Failed to find font table in config file: %s\n", config_path);
+    toml_free(config);
     return 0;
   }
 
   // Temporary string to store the font path
   char* font_path_str;
-
   if (toml_rtos(toml_raw_in(font_table, "path"), &font_path_str) == -1)
   {
-    fprintf(stderr, "Failed to find font path in config file: %s\n", CONFIG_FILE_PATH);
+    fprintf(stderr, "Failed to find font path in config file: %s\n", config_path);
+    toml_free(config);
     return 0;
   }
 
   font_path = strdup(font_path_str);
 
-  // Free the TOML and temp string memory
-  free((void*)font_path_str);
+  // Free the TOML and temporary string memory
+  free(font_path_str);
   toml_free(config);
   return 1;
 }
@@ -61,8 +76,8 @@ static void free_config()
   if (font_path != NULL)
   {
     free(font_path);
+    font_path = NULL;
   }
-  font_path = NULL;
 }
 
 #endif
