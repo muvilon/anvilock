285f10d8d7682e9a3b185216956010f459a27fff
nots1dd <sid9.karanam@gmail.com>
2025-05-03 16:19:09 +0530

    [CHORE] Convert term/OutputStream into header only logic

--- include/term/OutputStream.hpp
+++ include/term/OutputStream.hpp
@@ -15,21 +15,52 @@ struct StyledText
   std::string_view text;
   std::string_view styleSuffix;
 
-  friend auto operator<<(std::ostream& os, const StyledText& styled) -> std::ostream&;
+  friend auto operator<<(std::ostream& os, const StyledText& styled) -> std::ostream&
+  {
+      return os << styled.stylePrefix << styled.text << styled.styleSuffix;
+  }
 };
 
-// Combine multiple ANSI codes into a single escape sequence
-auto build_style_prefix(std::initializer_list<types::AnsiColor> styles) -> std::string;
+inline auto build_style_prefix(std::initializer_list<types::AnsiColor> styles) -> std::string
+{
+  std::string result;
+  for (const auto& style : styles)
+  {
+    result += style;
+  }
+  return result;
+}
 
-// Generic style combinator
-auto styleStream(std::string_view text, std::initializer_list<types::AnsiColor> styles)
-  -> StyledText;
+inline auto styleStream(std::string_view text, std::initializer_list<types::AnsiColor> styles)
+  -> StyledText
+{
+  return {
+    .stylePrefix = build_style_prefix(styles), .text = text, .styleSuffix = term::ansi::ansiReset};
+}
 
-// Shortcuts
-auto boldStream(std::string_view text) -> StyledText;
-auto underlineStream(std::string_view text) -> StyledText;
-auto colorStream(std::string_view text, types::AnsiColor color) -> StyledText;
-auto boldColorStream(std::string_view text, types::AnsiColor color) -> StyledText;
-auto underlineColorStream(std::string_view text, types::AnsiColor color) -> StyledText;
+inline auto boldStream(std::string_view text) -> StyledText
+{
+  return styleStream(text, {term::ansi::ansiBold});
+}
+
+inline auto underlineStream(std::string_view text) -> StyledText
+{
+  return styleStream(text, {term::ansi::ansiUnderline});
+}
+
+inline auto colorStream(std::string_view text, types::AnsiColor color) -> StyledText
+{
+  return styleStream(text, {color});
+}
+
+inline auto boldColorStream(std::string_view text, types::AnsiColor color) -> StyledText
+{
+  return styleStream(text, {term::ansi::ansiBold, color});
+}
+
+inline auto underlineColorStream(std::string_view text, types::AnsiColor color) -> StyledText
+{
+  return styleStream(text, {term::ansi::ansiUnderline, color});
+}
 
 } // namespace anvlk::term::ostream
